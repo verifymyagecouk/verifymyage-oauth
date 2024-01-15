5d6644e7c33ef33d5b0a8e499ddb89eb7483afa8
Guilherme Cezarini <cezarini@verifymy.io>
2024-01-15 10:35:32 -0300

    [feat] adding de2 to contries

--- src/Countries.php
+++ src/Countries.php
@@ -8,6 +8,7 @@ class Countries {
   const UNITED_KINGDOM_TWO = "gb2";
   const FRANCE = "fr";
   const GERMANY = "de";
+  const GERMANY_TWO = "de2";
   const UNITED_STATES_OF_AMERICA = "us";
   const UNITED_STATES_OF_AMERICA_TWO = "us2";
   const UNITED_STATES_OF_AMERICA_THREE = "us3";
