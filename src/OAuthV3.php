3b7de8a3849a516bf62cd8eb764c88aab1d88274
Guilherme Cezarini <cezarini@verifymy.io>
2024-10-03 15:36:24 -0300

    [feat] adding OAuthV3 to change Authorization header

---
+++ src/OAuthV3.php
@@ -0,0 +1,117 @@
+<?php
+
+
+namespace VerifyMyAge;
+
+use GuzzleHttp\Client;
+
+/**
+ * VerifyMyAge OAuth2 - Adult Content SDK
+ */
+class OAuthV3
+{
+    private $clientID;
+
+    private $clientSecret;
+
+    private $redirectURL;
+
+    private $currentProvider;
+
+    const METHODS = [
+        Methods::AGE_ESTIMATION,
+        Methods::CREDIT_CARD,
+        Methods::ID_SCAN,
+        Methods::ID_SCAN_FACE_MATCH,
+        Methods::EMAIL,
+    ];
+
+    const COUNTRIES = [
+        Countries::UNITED_KINGDOM,
+        Countries::FRANCE,
+        Countries::GERMANY,
+        Countries::UNITED_STATES_OF_AMERICA,
+        Countries::DEMO,
+    ];
+
+    public function __construct($clientID, $clientSecret, $redirectURL)
+    {
+        $this->clientID = $clientID;
+        $this->clientSecret = $clientSecret;
+        $this->redirectURL = $redirectURL;
+        $this->currentProvider = null;
+
+    }
+
+    /**
+     * If you're still in development stages, you can use our sandbox environment
+     */
+    public function useSandbox()
+    {
+        $this->provider()->useSandbox();
+    }
+
+    /**
+     * Do a post with HMAC authorization to VerifyMy OAuthV2 and return response from service.
+     */
+    public function getStartVerificationUrl(string $country, string $method="", string $businessSettingsId="", string $externalUserId="", string $verificationId="", bool $stealth=false, array $userInfo=array()){
+        if (!in_array($country, static::COUNTRIES)) {
+            throw new \Exception("Invalid country: " . $country);
+        }
+
+        if($method && !in_array($method, static::METHODS)){
+            throw new \Exception("Invalid method: ". $method);
+        }
+    
+        try {
+            $body = [
+                "scope"                 => $this->provider()->getDefaultScope(),
+                "country"               => $country,
+                "method"                => $method,
+                "business_settings_id"  => $businessSettingsId,
+                "external_user_id"      => $externalUserId,
+                "verification_id"       => $verificationId,
+                "redirect_url"          => $this->redirectURL,
+            ];
+            if (count($userInfo)) {
+                $body['user_info'] = $this->provider()->getUserInfoEncoded($userInfo);
+            }
+            $bodyEncoded        = json_encode($body);
+            $authorization      = $this->provider()->generateHMACAutorization($bodyEncoded);
+            $url                = $this->provider()->getBaseAuthorizationUrl();
+            $urlWithQueryParam  = "{$url}?stealth={$stealth}";
+            $client             = new Client();
+            $response           = $client->request('POST', $urlWithQueryParam, [
+                'headers' => [
+                    'Authorization'         => $authorization,
+                    'Content-Type'          => 'application/json',
+                ],
+                'body' => $bodyEncoded,
+    
+            ]);
+            $responseBodyDecode = json_decode($response->getBody()->getContents(), true);
+            return $responseBodyDecode;
+
+        }catch (\Exception $e) {
+            throw new \Exception("Error on get start verification url. Message: " . $e->getMessage());
+        
+        }
+       
+    }
+
+    /**
+     * VMA Provider
+     */
+    private function provider()
+    {
+        if ($this->currentProvider === null) {
+            $this->currentProvider = new Providers\VerifyMyAgeV3Provider([
+                'clientId'                 => $this->clientID,
+                'clientSecret'             => $this->clientSecret,
+                'redirectUri'              => $this->redirectURL,
+                
+            ]);
+        }
+        return $this->currentProvider;
+    }
+}
