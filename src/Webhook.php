069627781ad2b61424c6cb3d754b23a3b31a7353
Bruno de Sousa <{{your_github_email}}>
2026-01-28 17:34:56 +0000

    renaming minimum to minimal

--- src/Webhook.php
+++ src/Webhook.php
@@ -3,7 +3,7 @@
 namespace VerifyMyAge;
 
 class Webhook {
-    const MINIMUM_NOTIFICATION_LEVEL = "minimal";
+    const MINIMAL_NOTIFICATION_LEVEL = "minimal";
     const METHOD_EXHAUSTED_NOTIFICATION_LEVEL = "method_exhausted";
     const DETAILED_NOTIFICATION_LEVEL = "detailed";
 }
