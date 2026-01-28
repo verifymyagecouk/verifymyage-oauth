594c30e03a5ab1cf748217bc52123c448be31fac
Bruno de Sousa <{{your_github_email}}>
2026-01-28 17:32:02 +0000

    upgrading version

--- src/Webhook.php
+++ src/Webhook.php
@@ -3,7 +3,7 @@
 namespace VerifyMyAge;
 
 class Webhook {
-    const MINIMUM_NOTIFICATION_LEVEL = "minimum";
+    const MINIMUM_NOTIFICATION_LEVEL = "minimal";
     const METHOD_EXHAUSTED_NOTIFICATION_LEVEL = "method_exhausted";
     const DETAILED_NOTIFICATION_LEVEL = "detailed";
 }
